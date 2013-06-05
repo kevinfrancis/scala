acfca4e30a63f0adbcfa6149083db678abb4b4b2
Kevin Francis <kevinfran@gmail.com>
2013-06-05 16:32:15 -0700

    iterative permutations printing

--- src/main/scala/algos/Permutations.scala
+++ src/main/scala/algos/Permutations.scala
@@ -1,7 +1,10 @@
 package algos
 
-object Permutations {
-  
+/**
+ * Functional approach for finding permutations of a list
+ * Functional <==> Fun!
+ */
+object FunctionalPermutations {
   /* 
    * The Seq type would have been more appropriate instead of the List type
    * But adding to the head of a List is O(1) unlike adding to a Seq 
@@ -25,13 +28,57 @@ object Permutations {
     case true => List(List())
     case false => permutations(xs.tail) flatMap (p => insert(xs.head, p))
   }
-  
 
+}
+
+/**
+ * Permutations Printer
+ */
+object PermutationsPrinter {
+  
+  
+  private def swap[T](arr: Array[T], i: Int, j: Int): Unit = {
+    val tmp = arr(j)
+    arr(j)  = arr(i)
+    arr(i)  = tmp
+  }
+   
+  
+  /** 
+   * Uses iterative approach to printing
+   * Needs mutable array
+   */
+  def print_permutations[T](arr: Array[T]): Unit = {
+    
+    val length = arr.length
+    
+    def permute[T](start: Int): Unit = {
+      
+      if (start == length)
+        println(arr.toList)
+      
+      for (i <- Range(start, length)) {
+        swap(arr, start, i)
+        permute(start + 1)
+        swap(arr, start, i)
+      }
+    } 
+    
+    permute(0)
+      
+  }
+}
+
+object Permutations {
+  
   def main(args: Array[String]): Unit = {
-    println (permutations(List()))
-    println (permutations(List("single_item")))
-    println (permutations(List("x", "y")))
-    println (permutations(List(1, 2, 3)))
+    println (FunctionalPermutations.permutations(List()))
+    println (FunctionalPermutations.permutations(List("single_item")))
+    println (FunctionalPermutations.permutations(List("x", "y")))
+    println (FunctionalPermutations.permutations(List(1, 2, 3)))
+    
+    PermutationsPrinter.print_permutations(Array(1, 2, 3))
+    PermutationsPrinter.print_permutations("abcd".toArray)
   }
 
 }
